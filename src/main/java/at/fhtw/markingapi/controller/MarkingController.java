9b55673a7da1bcc3d8836a64c6f9d90681f4ae1d
Anna Till <ic21b041@technikum-wien.at>
2022-12-21 18:32:32 +0100

    create the RESTful infrastructure

---
+++ src/main/java/at/fhtw/markingapi/controller/MarkingController.java
@@ -0,0 +1,27 @@
+package at.fhtw.markingapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarkingController {
+    @GetMapping("api/marking")
+    public int calculateGrade(@RequestParam int percentage) {
+        if (0 <= percentage && percentage < 50) {
+            return 5;
+        }
+        else if (percentage < 63) {
+            return 4;
+        }
+        else if (percentage < 75) {
+            return 3;
+        }
+        else if (percentage < 88) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
+}
